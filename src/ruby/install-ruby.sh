f4e2bd45fce122caae3a1e4b60c060adcd5b1336
Stewart Park <hello@stewartjpark.com>
2016-06-13 17:52:39 -0700

    Fix ruby

--- src/ruby/install-ruby.sh
+++ src/ruby/install-ruby.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:/usr/local/rvm/bin 
-. "/usr/local/rvm/scripts/rvm"
-sudo rvm install ruby-2.3
+export PATH=$PATH:/usr/local/rvm/bin
+source "/usr/local/rvm/scripts/rvm"
+rvm install ruby-2.3
 rvm use ruby-2.3
