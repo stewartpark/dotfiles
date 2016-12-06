3f2f7c70c2f93c75b2e70b55d48cf4f1849471ec
Stewart Park <hello@stewartjpark.com>
2016-12-05 18:58:25 -0800

    Remove alias for nvim and make usable for linux

--- src/bash/inc/alias.sh
+++ src/bash/inc/alias.sh
@@ -1,5 +1,8 @@
-alias ls='ls -G'
+if [[ "`uname`" == "Linux" ]]; then
+    alias ls='ls --color=always'
+else
+    alias ls='ls -G'
+fi;
+
 alias tor-ssh='ssh -o "ProxyCommand nc -x 127.0.0.1:9050 %h %p"'
 alias tor-sftp='sftp -o "ProxyCommand nc -x 127.0.0.1:9050 %h %p"'
-alias vi='nvim'
-alias vim='nvim'
