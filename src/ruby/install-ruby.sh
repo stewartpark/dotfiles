0cd4c09675b19a68c6c16906b55d688ef0c26bac
Stewart Park <hello@stewartjpark.com>
2016-12-06 11:32:22 -0800

    Add pryrc

--- src/ruby/install-ruby.sh
+++ src/ruby/install-ruby.sh
@@ -9,6 +9,10 @@ rbenv global 2.3.1
 # To use 2.3 in the current session
 rbenv shell 2.3.1
 
-# Install bundler
+# Install bundler and pry
 rbenv exec gem install bundler
+rbenv exec gem install pry
 rbenv rehash
+
+# Install .pryrc
+ln -sf `pwd`/pryrc $HOME/.pryrc
