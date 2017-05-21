68e2627893e9c680596a9be79bf7f785329a7c41
Stewart Park <hello@stewartjpark.com>
2017-05-21 02:16:23 -0700

    Add some fix

--- src/python/install-python.sh
+++ src/python/install-python.sh
@@ -9,8 +9,8 @@ sudo pip install psutil netifaces  # For Powerline
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-pyenv install 3.5.1
-pyenv global 3.5.1
+pyenv install 3.5.2
+pyenv global 3.5.2
 
 # To use the new python in the current session
-pyenv shell 3.5.1
+pyenv shell 3.5.2
--- src/ruby/install-ruby.sh
+++ src/ruby/install-ruby.sh
@@ -3,11 +3,11 @@
 export PATH=$PATH:$HOME/.rbenv/bin
 eval $(rbenv init -)
 
-rbenv install 2.3.1
-rbenv global 2.3.1
+rbenv install 2.4.0
+rbenv global 2.4.0
 
 # To use 2.3 in the current session
-rbenv shell 2.3.1
+rbenv shell 2.4.0
 
 # Install bundler and pry
 rbenv exec gem install bundler
