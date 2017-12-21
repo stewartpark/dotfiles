55e307f636d5184eb05a5552d0cab7341b39bebe
Stewart Park <hello@stewartjpark.com>
2017-12-20 19:11:00 -0800

    Upgrade python/ruby's version

--- src/ruby/install-ruby.sh
+++ src/ruby/install-ruby.sh
@@ -3,11 +3,11 @@
 export PATH=$PATH:$HOME/.rbenv/bin
 eval $(rbenv init -)
 
-rbenv install 2.4.0
-rbenv global 2.4.0
+rbenv install 2.4.3
+rbenv global 2.4.3
 
 # To use 2.3 in the current session
-rbenv shell 2.4.0
+rbenv shell 2.4.3
 
 # Install bundler and pry
 rbenv exec gem install bundler
