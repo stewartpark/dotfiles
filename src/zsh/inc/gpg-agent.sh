90fa00e8ed02d1cc504335cf6599677c1f65949e
Stewart Park <hello@stewartjpark.com>
2018-11-05 16:42:36 -0800

    Use GPG Agent as SSH Agent

--- src/zsh/inc/gpg-agent.sh
+++ src/zsh/inc/gpg-agent.sh
@@ -1,2 +1,7 @@
 export GPG_TTY=$(tty)
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+
 eval $(keychain -q --noask --agents gpg --eval)
+
+# Add user-specific ssh keys to the emulated ssh agent by gpg agent.
+ssh-add 2> /dev/null
