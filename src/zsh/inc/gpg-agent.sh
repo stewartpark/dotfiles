b781a20b780996c720e7ac024b3ac9c194ec22b7
Stewart Park <hello@stewartjpark.com>
2020-05-03 00:50:40 -0700

    Fix gpg-agent and keychain

--- src/zsh/inc/gpg-agent.sh
+++ src/zsh/inc/gpg-agent.sh
@@ -1,7 +1,10 @@
 export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+export GPG_AGENT_INFO=$(gpgconf --list-dirs agent-socket)
 
-eval $(keychain -q --noask --agents gpg --eval)
+if [[ -z $(pidof ssh-agent) && -z $(pidof gpg-agent) ]]; then
+    eval $(keychain -q --noask --agents gpg --eval)
+fi
 
 # Add user-specific ssh keys to the emulated ssh agent by gpg agent.
 ssh-add 2> /dev/null
