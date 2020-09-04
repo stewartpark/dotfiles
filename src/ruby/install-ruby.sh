0a9b65425385dabb3001bd0c9f9fbb66691a092d
Stewart Park <stewart@Stewarts-MacBook-Pro.local>
2020-09-03 22:28:51 -0700

    Upgrade ruby

--- src/ruby/install-ruby.sh
+++ src/ruby/install-ruby.sh
@@ -3,11 +3,11 @@
 export PATH=$PATH:$HOME/.rbenv/bin
 eval $(rbenv init -)
 
-rbenv install 2.6.3
-rbenv global 2.6.3
+rbenv install 2.7.1
+rbenv global 2.7.1
 
 # To use the installed version in the current session
-rbenv shell 2.6.3
+rbenv shell 2.7.1
 
 # Install bundler and pry
 rbenv exec gem install bundler
