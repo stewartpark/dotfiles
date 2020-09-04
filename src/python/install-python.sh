6d655eec307aa7ed348b36ee19578b74f57cc9f9
Stewart Park <stewart@Stewarts-MacBook-Pro.local>
2020-09-03 22:28:14 -0700

    Retire vim

--- src/python/install-python.sh
+++ src/python/install-python.sh
@@ -2,15 +2,14 @@
 
 # System-wide Python set up
 sudo easy_install pip
-sudo pip install neovim  # Neovim Python support
 sudo pip install psutil netifaces  # For Powerline
 
 # Local setup with pyenv
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-pyenv install 3.7.4
-pyenv global 3.7.4
+pyenv install 3.8.5
+pyenv global 3.8.5
 
 # To use the new python in the current session
-pyenv shell 3.7.4
+pyenv shell 3.8.5
